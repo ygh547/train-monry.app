cc9614c6126b0e07704829b3b16e35c3b84ee2ea
ygh547 <ygh547@naver.com>
2022-06-07 14:53:28 +0900

    add

--- app_ml.py
+++ app_ml.py
@@ -47,16 +47,26 @@ def run_ml():
     elif Self_Employed == '아니다' :
         Self_Employed = 0
     ApplicantIncome = st.number_input('신청인의 연 평균 소득(만원)을 입력해주세요.',0,)
-    if ApplicantIncome >= 5000 :
-        ApplicantIncome = 1
+    if ApplicantIncome >= 3000 :
+        ApplicantIncome = 3
+    elif ApplicantIncome >= 2000 :
+        ApplicantIncome = 2
+    elif ApplicantIncome >= 1000 :
+        ApplicantIncome = 1    
     else :
         ApplicantIncome = 0
     CoapplicantIncome = st.number_input('부양자의 연 평균 소득(만원)을 입력해주세요.',0,)
     if CoapplicantIncome >= 3000 :
+        CoapplicantIncome = 3
+    if CoapplicantIncome >= 2000 :
+        CoapplicantIncome = 2
+    if CoapplicantIncome >= 1000 :
         CoapplicantIncome = 1
     else :
         CoapplicantIncome = 0
     LoanAmount = st.number_input('원하는 대출금액(만원)을 입력해주세요.',0,3000)
+    if LoanAmount >= 100 :
+        LoanAmount = 1
     Loan_Amount_Term = st.slider('납부기간을 정해주세요', 12, 360)
     Credit_History = st.radio('연체유무가 있으신가여?',['있다','없다'])
     if Credit_History == '없다' :
@@ -77,17 +87,21 @@ def run_ml():
         Loan_Status = 0 
 
     if st.button('대출금액 예측') : 
+        # 1. 신규 고객의 정보를 넘파이 어레이로 만들어준다.
         new_data = np.array([Gender,Married,Dependents ,Education ,Self_Employed ,ApplicantIncome ,CoapplicantIncome ,LoanAmount ,Loan_Amount_Term ,
         Credit_History ,Property_Area ])
+        # 2. 학습할때 사용한 X 의 피처 스케일러를 이용해서, 피처스케일링하기
+        # 먼저, 데이터를 2차원으로 만들어준다.
 
         new_data = new_data.reshape(1, 11)
         new_data = scaler_X.transform(new_data)
+        # 3. 인공지능에게 예측해달라고 한다.
         y_pred = regressor.predict(new_data)
-        # y_pred = scaler_y.inverse_transform(y_pred)
+        # 4. 예측한 값을, 원상복구 시킨다.
+        y_pred = scaler_y.inverse_transform(y_pred)
 
-        y_pred = [Gender + Married + Dependents + Education  + Self_Employed  + ApplicantIncome  + CoapplicantIncome  + LoanAmount  + 
-        Loan_Amount_Term + Credit_History  + Property_Area]
-        st.write('당신의 점수는'+ str(y_pred) + '점입니다.')
+        y_pred = round(y_pred[0,0])
+        st.write('대출 가능 금액은'+ str(y_pred) + '달러입니다.')
 
 
 
